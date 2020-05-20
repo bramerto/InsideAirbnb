ef2f4d9b0714b82f7c344773f4473865ab379fe1
Bram <bramerto@gmail.com>
2020-05-20 14:34:53 +0200

    Default chart.js implementation

---
+++ InsideAirbnb/InsideAirbnbApp/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using InsideAirbnbApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsideAirbnbApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize (Policy = "Admin")]
+        public ActionResult Index()
+        {
+            var lstModel = new List<DefaultChartViewModel>();
+            
+            return View(lstModel);
+        }
+    }
+}
---
+++ InsideAirbnb/InsideAirbnbApp/ViewModels/DefaultChartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InsideAirbnbApp.ViewModels
+{
+    public class DefaultChartViewModel
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+    }
+}
